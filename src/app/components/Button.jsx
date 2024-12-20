a83f58355252382e48183476e781fc0fc238408b
EzHavoc <kanishksingh778@gmail.com>
2024-12-20 19:25:39 +0530

    Main Landing Page

---
+++ src/app/components/Button.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import "../button.css";
+
+const GlowingButton = () => {
+  const handleClick = () => {
+    alert("Button Clicked!");
+  };
+
+  return (
+    <button onClick={handleClick} className="glowing-button">
+      Join Us!
+    </button>
+  );
+};
+
+export default GlowingButton;
