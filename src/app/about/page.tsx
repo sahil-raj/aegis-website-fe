9f8f0dfd893a0668cc1c211ac51e05655e42f894
karan2527 <karanjain2527@gmail.com>
2024-12-31 15:20:15 +0530

    commit again

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,4 @@
-// "use client";
+//"use client";
 import React from "react";
 import {Shield, Users, Target, Award} from "lucide-react";
 import "../globals.css";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import Navbar from "@/components/Navbar";
 import GlowingButton from "@/components/Button";
