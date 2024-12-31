401605fb83d14dcd8fbd3bef549005a034921151
sahil-raj <sahilrajaaru@gmail.com>
2025-01-01 04:23:54 +0530

    fix: allow images from all domains

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["drive.google.com", "www.sahilraj.xyz"],
+    domains: ["*"],
   },
 };
 
